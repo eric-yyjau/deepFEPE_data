b35a08eeed94b3f70c225ccad538d077b54a1bad
eric-yyjau <eric.yyjau@gmail.com>
2020-08-19 20:20:08 +0000

    update tum sequences

--- dump_tools/tum/download.py
+++ dump_tools/tum/download.py
@@ -7,13 +7,24 @@ import glob
 if __name__ == "__main__":
     # download
     sequences = [
-        "freiburg1/rgbd_dataset_freiburg1_desk",
-        "freiburg1/rgbd_dataset_freiburg1_desk2",
-        "freiburg1/rgbd_dataset_freiburg1_room",
-        "freiburg2/rgbd_dataset_freiburg2_desk",
-        "freiburg2/rgbd_dataset_freiburg2_xyz",
+        # "freiburg1/rgbd_dataset_freiburg1_desk",
+        # "freiburg1/rgbd_dataset_freiburg1_desk2",
+        # "freiburg1/rgbd_dataset_freiburg1_room",
+        # "freiburg2/rgbd_dataset_freiburg2_desk",
+        # "freiburg2/rgbd_dataset_freiburg2_xyz",
+        # "freiburg3/rgbd_dataset_freiburg3_long_office_household",
+        # "freiburg3/rgbd_dataset_freiburg3_nostructure_texture_far",
+        # all from the same undistorted camera: 
+        # training
         "freiburg3/rgbd_dataset_freiburg3_long_office_household",
-        "freiburg3/rgbd_dataset_freiburg3_nostructure_texture_far",
+        "freiburg3/rgbd_dataset_freiburg3_long_office_household_validation",
+        "freiburg3/rgbd_dataset_freiburg3_sitting_xyz",
+        "freiburg3/rgbd_dataset_freiburg3_structure_texture_far",
+        "freiburg3/rgbd_dataset_freiburg3_structure_texture_near",
+        "freiburg3/rgbd_dataset_freiburg3_teddy",
+        # testing
+        "freiburg3/rgbd_dataset_freiburg3_walking_xyz",
+        "freiburg3/rgbd_dataset_freiburg3_large_cabinet_validation",
     ]
     # wget https://vision.in.tum.de/rgbd/dataset/freiburg1/rgbd_dataset_freiburg1_desk.tgz
     # wget https://vision.in.tum.de/rgbd/dataset/freiburg1/rgbd_dataset_freiburg1_desk2.tgz
